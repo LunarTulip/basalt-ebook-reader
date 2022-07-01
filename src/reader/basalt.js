6d890863cd97ead843f209ab7a78a8e346d8da08
Alyssa Riceman <alyssariceman@gmail.com>
2022-07-01 15:35:47 -0400

    add basic book navigation

--- src/reader/basalt.js
+++ src/reader/basalt.js
@@ -1,13 +1,28 @@
 "use strict";
 
-document
-    .getElementById("choosefile")
-    .addEventListener("change", function() {
-        this.files[0].arrayBuffer().then(function(file) {
-            var book = ePub(file);
-            var rendition = book.renderTo("test", {
-                flow: "scrolled-doc"
-            });
-            var displayed = rendition.display();
-        })
+var book;
+var toc;
+var rendition;
+
+function set_toc_dropdowns(toc) {
+    let toc_dropdowns = Array.from(document.getElementsByClassName("tocDropdown"));
+    toc.forEach(toc_entry => {
+        let entry_element = document.createElement("option");
+        entry_element.setAttribute("value", toc_entry.id);
+        entry_element.textContent = toc_entry.label;
+        toc_dropdowns.forEach(dropdown => dropdown.appendChild(entry_element.cloneNode(true)));
     });
+}
+
+async function displayBook(file) {
+    book = ePub(file);
+    await book.opened;
+    set_toc_dropdowns(book.navigation.toc);
+    rendition = book.renderTo("book", {flow: "scrolled-doc", width: "100%"});
+    rendition.display();
+}
+
+// Temporary file-selector until I devise a more stable option
+document.getElementById("choosefile").addEventListener("change", function() {
+        this.files[0].arrayBuffer().then(displayBook)
+});
