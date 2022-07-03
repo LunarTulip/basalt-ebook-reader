5b7d0afa3e60cb53c2e2cb266177f224717f20c9
Alyssa Riceman <alyssariceman@gmail.com>
2022-07-03 09:39:25 -0400

    support nested tables of contents

--- src/reader/basalt.js
+++ src/reader/basalt.js
@@ -1,19 +1,33 @@
 "use strict";
 
 var book;
-var toc;
 var rendition;
 
+// toc_array: array of book TOC objects, either the top-level TOC or a descendant
+// parent_count: number
+function get_toc_items(toc_array, parent_count) {
+    let items = Array();
+    toc_array.forEach(toc_entry => {
+        let item_label = (String.fromCharCode(160).repeat(parent_count * 4)) + toc_entry.label.trim(); // The trim is to compensate for an epub.js bug at the cost of fidelity to sources whose TOCs *actually* have whitespace
+        items.push({label: item_label, link: toc_entry.href});
+        items = items.concat(get_toc_items(toc_entry.subitems, parent_count + 1));
+    });
+    return items;
+}
+
+// toc: book table of contents array
 function set_toc_dropdowns(toc) {
     let toc_dropdowns = Array.from(document.getElementsByClassName("tocDropdown"));
-    toc.forEach(toc_entry => {
+    let toc_items = get_toc_items(toc, 0);
+    toc_items.forEach(item => {
         let entry_element = document.createElement("option");
-        entry_element.setAttribute("value", toc_entry.id);
-        entry_element.textContent = toc_entry.label;
+        entry_element.setAttribute("value", item.link);
+        entry_element.textContent = item.label;
         toc_dropdowns.forEach(dropdown => dropdown.appendChild(entry_element.cloneNode(true)));
     });
 }
 
+// file: arrayBuffer containing an EPUB file
 async function displayBook(file) {
     book = ePub(file);
     await book.opened;
