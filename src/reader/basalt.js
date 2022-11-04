38e1916f9b523b4f163121c5830d573cec78b07b
Alyssa Riceman <alyssariceman@gmail.com>
2022-11-04 12:36:06 -0400

    minimal implementation

--- src/reader/basalt.js
+++ src/reader/basalt.js
@@ -1,29 +1,60 @@
 "use strict";
 
+/////////////////
+//   Globals   //
+/////////////////
+
 var book;
+var currentLocation = 0;
+var bookHtmlNode = document.getElementById("book");
 var rendition;
+
+/////////////////
+//   Display   //
+/////////////////
 
 // toc_array: array of book TOC objects, either the top-level TOC or a descendant
 // parent_count: number
-function get_toc_items(toc_array, parent_count) {
+function getTocItems(tocArray, parentCount) {
     let items = Array();
-    toc_array.forEach(toc_entry => {
-        let item_label = (String.fromCharCode(160).repeat(parent_count * 4)) + toc_entry.label.trim(); // The trim is to compensate for an epub.js bug at the cost of fidelity to sources whose TOCs *actually* have whitespace
-        items.push({label: item_label, link: toc_entry.href});
-        items = items.concat(get_toc_items(toc_entry.subitems, parent_count + 1));
+    tocArray.forEach(tocEntry => {
+        let itemLabel = (String.fromCharCode(160).repeat(parentCount * 4)) + tocEntry.label.trim(); // The trim is to compensate for an epub.js bug at the cost of fidelity to sources whose TOCs *actually* have whitespace
+        items.push({label: itemLabel, link: tocEntry.href});
+        items = items.concat(getTocItems(tocEntry.subitems, parentCount + 1));
     });
     return items;
 }
 
 // toc: book table of contents array
-function set_toc_dropdowns(toc) {
-    let toc_dropdowns = Array.from(document.getElementsByClassName("tocDropdown"));
-    let toc_items = get_toc_items(toc, 0);
-    toc_items.forEach(item => {
-        let entry_element = document.createElement("option");
-        entry_element.setAttribute("value", item.link);
-        entry_element.textContent = item.label;
-        toc_dropdowns.forEach(dropdown => dropdown.appendChild(entry_element.cloneNode(true)));
+async function setTocDropdowns(toc) {
+    let tocDropdowns = Array.from(document.getElementsByClassName("tocDropdown"));
+
+    // Clear any previously-set TOC
+    tocDropdowns.forEach(dropdown => {
+        while (dropdown.firstChild) {
+            dropdown.removeChild(dropdown.firstChild);
+        }
+    });
+
+    // Set new TOC
+    let tocItems = getTocItems(toc, 0);
+
+    await book.loaded.spine;
+    let firstSpineHref = book.spine.items[0].href;
+    let lastSpineHref = book.spine.items.at(-1).href;
+
+    if (tocItems[0].link != firstSpineHref) {
+        tocItems.unshift({label: "[Start]", link: firstSpineHref}); // Improve label?
+    }
+    if (tocItems.at(-1).link != lastSpineHref) {
+        tocItems.push({label: "[End]", link: lastSpineHref}); // Improve label?
+    }
+
+    tocItems.forEach(item => {
+        let entryElement = document.createElement("option");
+        entryElement.setAttribute("value", item.link);
+        entryElement.textContent = item.label;
+        tocDropdowns.forEach(dropdown => dropdown.appendChild(entryElement.cloneNode(true)));
     });
 }
 
@@ -31,10 +62,54 @@ function set_toc_dropdowns(toc) {
 async function displayBook(file) {
     book = ePub(file);
     await book.opened;
-    set_toc_dropdowns(book.navigation.toc);
-    rendition = book.renderTo("book", {flow: "scrolled-doc", width: "100%"});
+    setTocDropdowns(book.navigation.toc);
+
+    // currentLocation = 0;
+    // displaySection(currentLocation);
+
+    if(rendition) {
+        rendition.destroy();
+    }
+    rendition = book.renderTo("book", {flow: "scrolled-doc"});
     rendition.display();
 }
+
+////////////////////
+//   Navigation   //
+////////////////////
+
+function updateNavigation() {
+    let spineEntry = book.spine.get(rendition.currentLocation().start.cfi);
+    let tocEntry = book.navigation.get(spineEntry.href);
+
+    // Set TOC dropdowns to current location
+    if (tocEntry) {
+        Array.from(document.getElementsByClassName("tocDropdown")).forEach(dropdown => {
+            dropdown.value = tocEntry.href;
+            console.info(tocEntry.href);
+        });
+    } else {
+        let tocIndex = 0;
+        // Build map from spine entries to TOC entries and use that?
+    }
+}
+
+// href: href of section to go to
+function goToSectionFromTocDropdown(href) {
+    rendition.display(href).then(_ => updateNavigation());
+}
+
+function nextSection() {
+    rendition.next().then(_ => updateNavigation());
+}
+
+function prevSection() {
+    rendition.prev().then(_ => updateNavigation());
+}
+
+//////////////
+//   Main   //
+//////////////
 
 // Temporary file-selector until I devise a more stable option
 document.getElementById("choosefile").addEventListener("change", function() {
